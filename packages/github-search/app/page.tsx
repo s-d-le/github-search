fef6e8a53c6136810d764fa627c9bf1c639bddf6
Son Le <ayzkeys@gmail.com>
2024-09-07 23:21:26 +0200

    can now fetch user

--- packages/github-search/app/page.tsx
+++ packages/github-search/app/page.tsx
@@ -1,27 +1,46 @@
 "use client";
 import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { useForm, SubmitHandler } from "react-hook-form";
 import styles from "./page.module.css";
 
+type FormInputs = {
+  username: string;
+};
+
 export default function Home() {
+  const [error, setError] = useState<string | null>(null);
+  const [user, setUser] = useState<any | null>(null);
+
   const {
     register,
     handleSubmit,
     formState: { errors, isLoading },
-  } = useForm();
+  } = useForm<FormInputs>();
 
-  const search = () => {};
+  const onSubmit: SubmitHandler<FormInputs> = async (data) => {
+    try {
+      const response = await fetch(
+        `https://api.github.com/users/${data.username}`
+      );
+      setUser(response.json());
+      setError(null);
+    } catch (err) {
+      setUser(null);
+      setError("User not found or error occurred");
+    }
+  };
 
   return (
     <main className={styles.main}>
       <div>
-        <form onSubmit={handleSubmit(search)}>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div>
             <input
-              {...(register("userName"), { required: true })}
+              {...register("username", {
+                required: "You must enter an username",
+              })}
               type="text"
               placeholder="Looking for a Github user?"
-              name="userName"
               alt="Search for a Github user"
             />
             <button type="submit" disabled={isLoading}>
@@ -29,6 +48,8 @@ export default function Home() {
             </button>
           </div>
         </form>
+        {errors.username && <p>{errors.username.message}</p>}
+        {error && <p className="text-red-500">{error}</p>}
       </div>
     </main>
   );
